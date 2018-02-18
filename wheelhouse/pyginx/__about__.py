fbfe1800423b3060a21d05ea4c5d2562d01d8d9a
wrmsr <timwilloney@gmail.com>
2018-02-18 15:09:57 -0800

    --

---
+++ wheelhouse/pyginx/__about__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__title__ = 'pyginx'
+__version__ = '0.1.13.7.6'
+__description__ = ''
+__author__ = 'wrmsr'
+__author_email__ = 'timwilloney@gmail.com'
+__url__ = 'https://github.com/wrmsr/pyginx'
