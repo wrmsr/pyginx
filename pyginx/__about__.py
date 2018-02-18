931c7467c15ffb695f628d401f6aa54ea7df6cba
wrmsr <timwilloney@gmail.com>
2018-02-17 19:08:21 -0800

    --

--- pyginx/__about__.py
+++ pyginx/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'pyginx'
-__version__ = '0.1.dev0'
+__version__ = '0.1.13.7.0'
 __description__ = ''
 __author__ = 'wrmsr'
 __url__ = 'https://github.com/wrmsr/baseband'
